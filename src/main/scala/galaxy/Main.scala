2a3a189d2d91097a1538e89786460e16d9f262a2
Viktor Tanyi <tanyi.viktor@gmail.com>
2020-05-04 12:49:47 +0200

    Initial LWJGL setup

---
+++ src/main/scala/galaxy/Main.scala
@@ -0,0 +1,72 @@
+package galaxy
+
+import org.lwjgl.glfw._
+import org.lwjgl.opengl._
+import org.lwjgl.glfw.Callbacks._
+import org.lwjgl.glfw.GLFW._
+import org.lwjgl.opengl.GL11._
+import org.lwjgl.system.MemoryStack._
+import org.lwjgl.system.MemoryUtil._
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val window = createWindow()
+    GL.createCapabilities()
+    loop(window)
+
+    glfwFreeCallbacks(window)
+    glfwDestroyWindow(window)
+    glfwTerminate()
+    glfwSetErrorCallback(null).free()
+  }
+
+  private def createWindow(): Long = {
+    GLFWErrorCallback.createPrint(System.err).set
+    if (!glfwInit) {
+      throw new IllegalStateException("Unable to initialize GLFW")
+    }
+    glfwDefaultWindowHints()
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE)
+
+    val window = glfwCreateWindow(1728, 972, "Hello World!", NULL, NULL)
+    if (window == NULL) {
+      throw new RuntimeException("Failed to create the GLFW window")
+    }
+    glfwSetKeyCallback(window, (window, key, _, action, _) =>
+      if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+        glfwSetWindowShouldClose(window, true)
+      }
+    )
+
+    val stack = stackPush
+    try {
+      val pWidth = stack.mallocInt(1)
+      val pHeight = stack.mallocInt(1)
+      glfwGetWindowSize(window, pWidth, pHeight)
+      val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor)
+      val x = (vidmode.width - pWidth.get(0)) / 2
+      val y = (vidmode.height - pHeight.get(0)) / 2
+      glfwSetWindowPos(window, x, y)
+    } finally {
+      if (stack != null) stack.close()
+    }
+
+    glfwMakeContextCurrent(window)
+    glfwSwapInterval(1)
+    glfwShowWindow(window)
+
+    window
+  }
+
+  private def loop(window: Long): Unit = {
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
+
+    while (!glfwWindowShouldClose(window)) {
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+      glfwSwapBuffers(window)
+      glfwPollEvents()
+    }
+  }
+}
